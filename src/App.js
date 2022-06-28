c867b8287e96256c4d65aac955d57786707a4765
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-06-28 12:48:00 +0600

    mixin and include

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import "./styles/App.css";
 const App = () => {
   return (
     <div>
+      {/* title  */}
       <h2 className="title">CSS SASS WITH REACT</h2>
 
+      {/* menu  */}
       <div className="menu">
         <ul>
           <li>
@@ -17,6 +19,16 @@ const App = () => {
             <a href="#contact">Contact</a>
           </li>
         </ul>
+      </div>
+
+      {/* paragraph  */}
+      <div>
+        <p className="paragraph">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+          doloremque ratione quaerat, expedita laboriosam id quibusdam. Modi,
+          totam excepturi? Consequuntur quia veritatis ab dolore ea vero nisi
+          incidunt facere temporibus.
+        </p>
       </div>
     </div>
   );
