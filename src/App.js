7bc9256fa8d6657b709bdbfa65d9630ae9a1799c
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-06-28 12:13:41 +0600

    SASS Variable and nested styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,20 @@ const App = () => {
   return (
     <div>
       <h2 className="title">CSS SASS WITH REACT</h2>
+
+      <div className="menu">
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
